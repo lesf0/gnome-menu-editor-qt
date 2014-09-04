34ca08ac3f9b200e5988ef4d2c45d995462ee897
Denis Sheremet <zaycakitayca@xaker.ru>
2014-09-04 10:55:54 +0600

    minor bug fixes, editing single .desktop file enabled

--- main.cpp
+++ main.cpp
@@ -1,11 +1,1 @@
-#include "mainwindow.h"
-#include <QApplication>
-
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-
-    return a.exec();
-}
+/home/zaycakitayca/Devel/qt/gnome-menu-editor-qt/main.cpp
